0b9fa039f584839564445fde2f820335e3716c34
Giovanni Campagna <gcampagn@cs.stanford.edu>
2015-08-16 15:54:19 -0700

    cloud: add Android integration
    When running inside the app and the user logs in or successfully
    registers, propagate the cloud ID and auth token settings to
    the Android Java code (which will forward them to the node.js
    thread and from there the engine will know how to connect to
    the cloud and sync itself.

--- platform/cloud/public/javascripts/shared.js
+++ platform/cloud/public/javascripts/shared.js
@@ -1,3 +1,17 @@
 (function() {
     window.ThingEngine = {};
-})()
+
+    ThingEngine.setCloudId = function(cloudId, authToken) {
+        if (window.Android !== undefined)
+            Android.setCloudId(cloudId, authToken);
+        else
+            console.log('Setting cloud ID and auth token: ' + cloudId + ',' + authToken);
+    };
+
+    ThingEngine.setCloudIdWhenReady = function() {
+        $(function() {
+            ThingEngine.setCloudId($('#cloud-id').text(),
+                                   $('#auth-token').text());
+        });
+    }
+})();
