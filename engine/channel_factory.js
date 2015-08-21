23ad59de4f15e4526075050b16f7ebd854beab40
Giovanni Campagna <gcampagn@cs.stanford.edu>
2015-08-21 00:05:16 -0700

    Kill old IFTTT code
    This code was never tested, likely buggy, and was written just
    to prove that the model was not inferior to old rulepedia - which
    probably was never disputed because the new model is designed
    to be more powerful. And it was creating confusion in reviewers
    trying to learn the system.

--- engine/channel_factory.js
+++ engine/channel_factory.js
@@ -23,7 +23,6 @@ module.exports = new lang.Class({
 
     _init: function(engine, tiers) {
         this._engine = engine;
-        this._channelMetas = {};
         this._cachedChannels = {};
 
         this._downloader = new ModuleDownloader('channels');
@@ -32,74 +31,8 @@ module.exports = new lang.Class({
         this._pipeManager = new PipeManager(tiers, this._proxyManager);
     },
 
-    _loadFromData: function(data) {
-        var channels = JSON.parse(data);
-        for (var i = 0; i < channels.length; i++) {
-            this._channelMetas[channels[i].id] = channels[i];
-        }
-    },
-
-    _loadFromDisk: function() {
-        var cacheFile = platform.getCacheDir() + '/channels.json';
-        return Q.try(function() {
-            var stats = fs.statSync(cacheFile);
-            if ((new Date).getTime() - stats.mtime.getTime() > 2 * 24 * 3600 * 1000) { // two days
-                fs.unlinkSync(cacheFile);
-                var e = new Error('Too old');
-                e.code = 'ENOENT';
-                throw e;
-            }
-        }).then(function() {
-            return Q.nfcall(fs.readFile, cacheFile);
-        }).then(function(data) {
-            this._loadFromData(data);
-        }.bind(this));
-    },
-
-    _loadFromServer: function() {
-        var cacheFile = platform.getCacheDir() + '/channels.json';
-        return Q.Promise(function(callback, errback) {
-            var buffer = '';
-            http.get(Config.THINGPEDIA_URL + '/db/channels.json', function(response) {
-                if (response.statusCode != 200) {
-                    errback(new Error('Unexpected HTTP error ' + response.statusCode + ' reading channel DB'));
-                    return;
-                }
-
-                response.setEncoding('utf8');
-                response.on('data', function(chunk) {
-                    buffer += chunk;
-                });
-                response.on('end', function() {
-                    callback(buffer);
-                });
-            }).on('error', function(error) {
-                errback(error);
-            });
-        }).then(function(data) {
-            return Q.nfcall(fs.writeFile, cacheFile, data, { mode: 0600 })
-                .then(function() {
-                    return this._loadFromData(data);
-                }.bind(this));
-        }.bind(this));
-    },
-
     load: function() {
-        return this._loadFromDisk().catch(function(e) {
-            if (e.code != 'ENOENT')
-                throw e;
-
-            return this._loadFromServer();
-        }.bind(this));
-    },
-
-    // For compatibility with existing channels, should only be called
-    // by the IFTTT code
-    createIFTTTChannel: function(id) {
-        if (id in this._cachedChannels)
-            return this._cachedChannels[id];
-        else
-            throw new Error('Invalid channel id ' + id);
+        return Q();
     },
 
     _getProxyChannel: function(forChannel, args) {
