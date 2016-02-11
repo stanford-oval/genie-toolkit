2c6161fe825db6a3a59948c4e03b250333eff471
Giovanni Campagna <gcampagn@cs.stanford.edu>
2016-02-10 17:06:44 -0800

    platform/cloud: refactor /thingpedia/install to use direct database
    Now that we have direct database access to ThingPedia, we don't
    need to go through a server-to-server request.

--- platform/cloud/routes/thingpedia.js
+++ platform/cloud/routes/thingpedia.js
@@ -8,64 +8,73 @@
 
 const Q = require('q');
 const express = require('express');
-const url = require('url');
-const http = require('http');
-const https = require('https');
-const path = require('path');
-const fs = require('fs');
 
+const db = require('../util/db');
 const user = require('../util/user');
+const feeds = require('../../shared/util/feeds');
+const model = require('../model/app');
+
 const EngineManager = require('../enginemanager');
 
 const ThingTalk = require('thingtalk');
 const AppCompiler = ThingTalk.Compiler;
 
-const THINGPEDIA_ORIGIN = 'https://thingpedia.herokuapp.com';
-//const THINGPEDIA_ORIGIN = 'http://127.0.0.1:5000';
-
 var router = express.Router();
 
-function getModule(parsed) {
-    if (parsed.protocol === 'https:')
-        return https;
-    else
-        return http;
+function SchemaRetriever() {
+    this._request = null;
+    this._pendingRequests = [];
 }
 
-function httpRequest(to) {
-    var parsed = url.parse(to);
-    parsed.method = 'GET';
-    return Q.Promise(function(callback, errback) {
-        var req = getModule(parsed).get(parsed, function(res) {
-            if (res.statusCode == 404)
-                return errback(new Error('No such app'));
-            if (res.statusCode != 200)
-                return errback(new Error('Unexpected HTTP error ' + res.statusCode));
+SchemaRetriever.prototype._ensureRequest = function() {
+    if (this._request !== null)
+        return;
 
-            var data = '';
-            res.setEncoding('utf8');
-            res.on('data', function(chunk) {
-                data += chunk;
+    this._request = Q.delay(0).then(function() {
+        var pending = this._pendingRequests;
+        this._pendingRequests = [];
+
+        return db.withClient(function(dbClient) {
+            return schema.getTypesByKinds(dbClient, pending, null);
+        }).then(function(rows) {
+            var obj = {};
+
+            rows.forEach(function(row) {
+                if (row.types === null)
+                    return;
+                obj[row.kind] = {
+                    triggers: row.types[0],
+                    actions: row.types[1]
+                };
             });
-            res.on('end', function() {
-                callback(data);
-            });
+
+            return obj;
         });
-        req.on('error', errback);
+    }.bind(this));
+};
+
+SchemaRetriever.prototype.getSchema = function(kind) {
+    if (this._pendingRequests.indexOf(kind) < 0)
+        this._pendingRequests.push(kind);
+    this._ensureRequest();
+    return this._request.then(function(everything) {
+        if (kind in everything)
+            return everything[kind];
+        else
+            return null;
     });
-}
+};
+
+var _schemaRetriever = new SchemaRetriever();
 
 router.get('/install/:id(\\d+)', user.redirectLogIn, function(req, res, next) {
-    httpRequest(THINGPEDIA_ORIGIN + '/api/code/apps/' + req.params.id)
-        .then(function(response) {
-            var parsed = JSON.parse(response);
-            if (parsed.error)
-                throw new Error(parsed.error);
-
-            // sanity check the app for version incompatibilities
-            var compiler = new AppCompiler();
-            compiler.compileCode(parsed.code);
-
+    db.withClient(function(dbClient) {
+        return model.get(dbClient, req.params.id);
+    }).then(function(app) {
+        // sanity check the app for version incompatibilities
+        var compiler = new AppCompiler();
+        compiler.setSchemaRetriever(_schemaRetriever);
+        return compiler.compileCode(app.code).then(function() {
             var params = Object.keys(compiler.params).map(function(k) {
                 return [k, compiler.params[k]];
             });
@@ -73,37 +82,7 @@ router.get('/install/:id(\\d+)', user.redirectLogIn, function(req, res, next) {
             return Q.try(function() {
                 if (compiler.feedAccess) {
                     return EngineManager.get().getEngine(req.user.id).then(function(engine) {
-                        return engine.messaging.getFeedMetas().then(function(feeds) {
-                            return feeds.filter(function(f) {
-                                return f.hasWriteAccess && f.kind === null;
-                            });
-                        }).tap(function(feeds) {
-                            return Q.all(feeds.map(function(f) {
-                                // at first sight, you might complain that this "modify in place"
-                                // would corrupt the database
-                                // but there is a RPC layer in the middle saving us: we only operate
-                                // on a copy of feeds so everything is fine
-                                if (f.name)
-                                    return;
-                                if (f.members.length === 1) {
-                                    f.name = "You";
-                                    return;
-                                }
-                                if (f.members.length === 2) {
-                                    if (f.members[0] === 1) {
-                                        return engine.messaging.getUserById(f.members[1]).then(function(u) {
-                                            f.name = u.name;
-                                        });
-                                    } else {
-                                        return engine.messaging.getUserById(f.members[0]).then(function(u) {
-                                            f.name = u.name;
-                                        });
-                                    }
-                                } else {
-                                    f.name = "Unnamed (multiple partecipants)";
-                                }
-                            }));
-                        });
+                        return feeds.getFeedList(engine, true);
                     });
                 } else {
                     return null;
@@ -113,15 +92,16 @@ router.get('/install/:id(\\d+)', user.redirectLogIn, function(req, res, next) {
                                             csrfToken: req.csrfToken(),
                                             thingpediaId: req.params.id,
                                             params: params,
-                                            name: parsed.name,
-                                            description: parsed.description,
+                                            name: app.name,
+                                            description: app.description,
                                             feeds: feeds,
-                                            code: parsed.code });
+                                            code: app.code });
             });
-        }).catch(function(e) {
-            res.status(400).render('error', { page_title: "ThingEngine - Error",
-                                              message: e.message });
-        }).done();
+        });
+    }).catch(function(e) {
+        res.status(400).render('error', { page_title: "ThingEngine - Error",
+                                          message: e.message });
+    }).done();
 });
 
 module.exports = router;
