70f447253ac2275769eb32149c83de0265ec6669
Giovanni Campagna <gcampagn@cs.stanford.edu>
2016-03-04 14:18:54 -0800

    Stop syncing apps and keywords between tiers
    Tiers were a terrible idea that lead to considerable complexity and
    lots of bugs. We will keep them for devices for the mobile client
    but we don't need them for apps or keywords.

--- lib/db/schema.sql
+++ lib/db/schema.sql
@@ -1,23 +1,12 @@
 drop table if exists app ;
-drop table if exists app_journal ;
 
 create table app (
        uniqueId varchar(255) primary key,
-       tier text not null,
        code text not null,
        state text not null,
        name text null default null,
        description text null default null
 );
-
-create table app_journal (
-       uniqueId varchar(255) primary key,
-       lastModified datetime
-);
-
-drop index if exists app_lastModified ;
-
-create index app_lastModified on app_journal(lastModified) ;
 
 drop table if exists device ;
 drop table if exists device_journal ;
@@ -37,14 +26,8 @@ drop index if exists device_lastModified;
 create index device_lastModified on device_journal(lastModified) ;
 
 drop table if exists keyword ;
-drop table if exists keyword_journal ;
 
 create table keyword (
        uniqueId varchar(255) primary key,
        value text default null
-);
-
-create table keyword_journal (
-       uniqueId varchar(255) primary key,
-       lastModified datetime
 );
