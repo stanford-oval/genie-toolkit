0ab7f2f3659261da134e7419d3a1d7fda412aeb2
Giovanni Campagna <gcampagn@cs.stanford.edu>
2018-04-28 16:47:04 -0700

    Add more tests for devices

--- test/index.js
+++ test/index.js
@@ -15,7 +15,11 @@ process.on('unhandledRejection', (up) => { throw up; });
 
 const assert = require('assert');
 
+const ThingTalk = require('thingtalk');
+const Ast = ThingTalk.Ast;
+
 const Engine = require('../lib/engine');
+const DeviceView = require('../lib/devices/device_view');
 
 // make all errors fatal
 const originalconsoleerror = console.error;
@@ -64,12 +68,16 @@ const FAILURE = {};
 function testDevices(engine) {
     const devices = engine.devices;
 
+    // test that the built devices are what we expect
+
     assert(devices.hasDevice('thingengine-own-desktop'));
     assert(devices.hasDevice('thingengine-own-global'));
     assert(devices.hasDevice('org.thingpedia.builtin.test'));
     assert(!devices.hasDevice('org.thingpedia.builtin.thingengine.phone'));
     assert(!devices.hasDevice('org.thingpedia.builtin.thingengine.home'));
     assert(!devices.hasDevice('org.thingpedia.builtin.thingengine.gnome'));
+
+    // test looking up devices
 
     const builtin = devices.getAllDevicesOfKind('org.thingpedia.builtin.thingengine.builtin');
     assert.strictEqual(builtin.length, 1);
@@ -82,11 +90,108 @@ function testDevices(engine) {
     assert.deepStrictEqual(devices.getAllDevicesOfKind('messaging'), []);
     assert.deepStrictEqual(devices.getAllDevicesOfKind('com.xkcd'), []);
 
+    // test device views, adding and removing devices
+
+    let added = FAILURE;
+    let removed = SUCCESS;
+    const view = new DeviceView(devices, Ast.Selector.Device('com.xkcd', null, null));
+    view.on('object-added', (d) => {
+        assert.strictEqual(d, devices.getDevice('com.xkcd'));
+        added = SUCCESS;
+    });
+    view.on('object-removed', () => {
+        removed = FAILURE;
+    });
+    view.start();
+    assert.deepStrictEqual(view.values(), []);
+
+    let view2;
+
     return devices.loadOneDevice({ kind: 'com.xkcd' }, true).then((device) => {
         const xkcd = devices.getAllDevicesOfKind('com.xkcd');
         assert.strictEqual(xkcd.length, 1);
         assert.strictEqual(xkcd[0], device);
         assert.strictEqual(devices.getDevice('com.xkcd'), device);
+
+        const viewvalues = view.values();
+        assert.strictEqual(viewvalues.length, 1);
+        assert.strictEqual(viewvalues[0], device);
+
+        assert.strictEqual(added, SUCCESS);
+        assert.strictEqual(removed, SUCCESS);
+
+        view.stop();
+        view2 = new DeviceView(devices, Ast.Selector.Device('com.xkcd', null, null));
+        // start the view before we connect to the signal, so we are sure not to receive it
+        view2.start();
+        view2.on('object-added', (d) => {
+            assert.strictEqual(d, devices.getDevice('com.xkcd'));
+            added = FAILURE;
+        });
+        view2.on('object-removed', () => {
+            removed = SUCCESS;
+        });
+
+        return devices.removeDevice(device);
+    }).then(() => {
+        assert.strictEqual(added, SUCCESS);
+        assert.strictEqual(removed, SUCCESS);
+
+        assert.deepStrictEqual(view2.values(), []);
+
+        const test = devices.getDevice('org.thingpedia.builtin.test');
+
+        return test.get_get_data({ count: 2, size: 10 });
+    }).then((result) => {
+        assert.deepStrictEqual(result, [{
+            data: '!!!!!!!!!!',
+        }, {
+            data: '""""""""""'
+        }]);
+
+        const fakeState = {
+            _state: {},
+            get(key) {
+                return this._state[key];
+            },
+            set(key, v) {
+                this._state[key] = v;
+                return v;
+            }
+        };
+
+        return new Promise((resolve, reject) => {
+            const test = devices.getDevice('org.thingpedia.builtin.test');
+
+            const stream = test.subscribe_get_data({ size: 10 }, fakeState);
+            const buffer = [];
+
+            stream.on('data', (d) => {
+                assert(d.hasOwnProperty('__timestamp'));
+                delete d.__timestamp;
+                buffer.push(d);
+            });
+            stream.on('end', () => reject(new Error('Unexpected end')));
+            stream.on('error', reject);
+
+            let atTimeout = null;
+            setTimeout(() => {
+                assert(buffer.length >= 4 && buffer.length <= 6);
+                stream.destroy();
+                atTimeout = buffer.length;
+            }, 5000);
+            setTimeout(() => {
+                assert.strictEqual(buffer.length, atTimeout);
+                assert.deepStrictEqual(buffer.slice(0, 4), [
+                    { data: '!!!!!!!!!!' },
+                    { data: '""""""""""' },
+                    { data: '##########' },
+                    { data: '$$$$$$$$$$' }
+                ]);
+
+                resolve();
+            }, 10000);
+        });
     });
 }
 
