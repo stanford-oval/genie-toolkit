861208460057d5a445bf2d6739832c6067cd2cb4
Silei Xu <ray.slxu@gmail.com>
2019-06-15 10:06:01 -0700

    minor

--- test/auto_test_almond.js
+++ test/auto_test_almond.js
@@ -652,7 +652,7 @@ const TEST_CASES = [
     [
     { code: ['executor', '=', 'USERNAME_0', ':', 'now', '=>', '@com.twitter.post'],
       entities: { USERNAME_0: 'mom' } },
-`>> You need a Matrix account: I talk to other Almonds via secure Matrix messaging service.
+`>> You need a Matrix account: I talk to other Almonds via the secure Matrix messaging service.
 >> link: Register a new Matrix account now https://riot.im/app/#/register
 >> Insert your Matrix username:
 >> ask special raw_string
