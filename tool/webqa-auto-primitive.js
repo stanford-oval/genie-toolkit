24a15f0cd04ad2f900c20bd8b75209772dddda91
Giovanni Campagna <gcampagn@cs.stanford.edu>
2019-10-08 20:19:05 -0700

    auto-primitive: apply PPDB transformations too
    Use bing to see which PPDB transformations are meaningful and
    which are not

--- tool/webqa-auto-primitive.js
+++ tool/webqa-auto-primitive.js
@@ -21,6 +21,7 @@ const Inflectors = require('en-inflectors').Inflectors;
 const ThingTalk = require('thingtalk');
 const Ast = ThingTalk.Ast;
 
+const BinaryPPDB = require('../lib/binary_ppdb');
 const { choose } = require('../lib/random');
 const { clean } = require('../lib/utils');
 const StreamUtils = require('../lib/stream-utils');
@@ -74,6 +75,16 @@ function sample(table, data, propertyChain, propertyType, rng) {
     return chosen.map((c) => String(c));
 }
 
+function getPlural(propertyCanonical) {
+    const words = propertyCanonical.split(' ');
+    if (words.length !== 2 || words[1] !== 'count')
+        return null;
+
+    const inflector = new Inflectors(words[0]);
+    const plural = inflector.toPlural();
+    return plural;
+}
+
 function getEdIng(propertyCanonical) {
     const words = propertyCanonical.split(' ');
     if (words.length > 1)
@@ -119,19 +130,23 @@ function phraseStartWithVerb(propertyCanonical) {
 const DO_THE_SEARCH = true;
 
 async function bingSearchSingle(query, cache) {
-    if (query in cache)
-        return cache[query];
 
     if (DO_THE_SEARCH) {
         const url = 'https://api.cognitive.microsoft.com/bing/v5.0/search';
-        const payload = {'q': '"' + query + '"'};
+        const completeQuery = '\'"' + query + '"\'';
+        //const completeQuery = 'inbody:' + query.split(/\s+/g).join('+');
+
+        if (completeQuery in cache)
+            return cache[completeQuery];
+
+        const payload = {'q': completeQuery };
         const key = process.env.BING_KEY;
         const headers = {'Ocp-Apim-Subscription-Key': key};
 
         const response = JSON.parse(await Tp.Helpers.Http.get(url + '?' + qs.stringify(payload), {
             extraHeaders: headers
         }));
-        return cache[query] = (response.webPages || {}).totalEstimatedMatches || 0;
+        return cache[completeQuery] = (response.webPages || {}).totalEstimatedMatches || 0;
     } else {
         console.log(`would search for ${query}`);
 
@@ -146,7 +161,7 @@ async function bingSearch(query, valueList, cache) {
         const newQuery = query.replace('${value}', value); //'
 
         let result = await bingSearchSingle(newQuery, cache);
-        result = result * Math.pow(10, query.split(/\s+/g).length); // length factor
+        result = result * Math.pow(2, query.split(/\s+/g).length); // length factor
 
         tot += result;
     }
@@ -171,6 +186,7 @@ const EQUAL_PATTERNS = [
 
     ['${table} ${propVerb} ${value}'],
     ['${table} with ${value} ${propNoun}'],
+    ['${table} with ${value} ${propPlural}'],
     ['${table} ${propVerb} ${value} ${propNoun}'],
     ['${table} ${propNoun} ${table}'],
 
@@ -181,6 +197,7 @@ const EQUAL_PATTERNS = [
 const COMPARATIVE_MORE_PATTERNS = [
     ['${table} with at least ${value} ${property}'],
     ['${table} with more than ${value} ${property}'],
+    ['${table} with more than ${value} ${propPlural}'],
 
     ['${table} ${propEd} at least ${value}'],
     ['${table} ${propEd} more than ${value}'],
@@ -192,6 +209,7 @@ const COMPARATIVE_MORE_PATTERNS = [
 const COMPARATIVE_LESS_PATTERNS = [
     ['${table} with at most ${value} ${property}'],
     ['${table} with less than ${value} ${property}'],
+    ['${table} with less than ${value} ${propPlural}'],
 
     ['${table} ${propEd} at most ${value}'],
     ['${table} ${propEd} less than ${value}'],
@@ -200,52 +218,98 @@ const COMPARATIVE_LESS_PATTERNS = [
     ['${table} ${propIng} less than ${value}'],
 ];
 
-const THRESHOLD = 10000;
+function getPPDBCandidates(ppdb, canonical) {
+    if (!ppdb)
+        return [canonical];
 
-async function applyPatterns(functionDef, argDef, valueList, patternList, operator, dataset, cache) {
+    const words = canonical.split(' ');
+    const output = [canonical];
+
+    for (let i = 0; i < words.length; i++) {
+        for (let j = i+1; j <= words.length; j++) {
+            const span = words.slice(i, j).join(' ');
+            const paraphrases = ppdb.get(span);
+            console.error(`ppdb ${span}:`, paraphrases);
+            if (paraphrases) {
+                for (let paraphrase of paraphrases)
+                    output.push([...words.slice(0, i), paraphrase, ...words.slice(j)].join(' '));
+            }
+        }
+    }
+
+    return output;
+}
+
+const THRESHOLD = 500000;
+
+async function applyPatterns(ppdb, functionDef, argDef, valueList, patternList, operator, dataset, cache) {
     /*if (propertyName === 'name')
         return [['value', 2], ['value_table', 1]];
 
     if (propertyName === 'geo')
         return [['table_near_value', 2], ['table_around_value', 1]];*/
 
-    const tableCanonical = functionDef.metadata.canonical || clean(functionDef.name);
+    const tableCanonicals = getPPDBCandidates(ppdb, functionDef.canonical || clean(functionDef.name));
     const propertyName = argDef.name;
     const propertyType = argDef.type;
-    const propertyCanonical = argDef.canonical;
+    const propertyCanonicals = getPPDBCandidates(ppdb, argDef.canonical);
 
-    const patterns = (await Promise.all(patternList.map(async ([pattern, condition]) => {
+    // special properties we don't want to handle
+    if (propertyName === 'name' || propertyName === 'geo' || /^address\.?/.test(propertyName))
+        return;
+
+    const dot = propertyName.lastIndexOf('.');
+    const lastProp = propertyName.substring(dot+1);
+
+    const patterns = [];
+    await Promise.all(patternList.map(async ([pattern, condition]) => {
         if (condition && !condition(functionDef.name, propertyName, propertyType))
-            return null;
+            return;
 
-        pattern = pattern.replace('${table}', tableCanonical);
-        pattern = pattern.replace('${property}', propertyCanonical);
+        for (let tableCanonical of tableCanonicals) {
+            for (let propertyCanonical of propertyCanonicals) {
+                let searchQuery = pattern;
 
-        const [propEd, propIng] = getEdIng(propertyCanonical);
-        const [propVerb, propNoun] = phraseStartWithVerb(propertyCanonical);
+                searchQuery = searchQuery.replace('${table}', tableCanonical);
+                searchQuery = searchQuery.replace('${property}', propertyCanonical);
 
-        const variants = { propEd, propIng, propVerb, propNoun };
-        for (let variantKey in variants) {
-            const variant = variantKey[variants];
-            if (variant)
-                pattern = pattern.replace('${' + variantKey + '}', variant);
-            else if (pattern.indexOf('${' + variantKey + '}') >= 0)
-                return null;
+                const propPlural = getPlural(propertyCanonical);
+                const [propEd, propIng] = getEdIng(propertyCanonical);
+                const [propVerb, propNoun] = phraseStartWithVerb(propertyCanonical);
+
+                const variants = { propEd, propIng, propVerb, propNoun, propPlural };
+
+                let ok = true;
+                for (let variantKey in variants) {
+                    const variant = variants[variantKey];
+                    if (variant) {
+                        searchQuery = searchQuery.replace('${' + variantKey + '}', variant);
+                    } else if (searchQuery.indexOf('${' + variantKey + '}') >= 0) {
+                        //console.log('missing ' + variantKey + ' for ' + propertyCanonical);
+                        ok = false;
+                        break;
+                    }
+                }
+                if (!ok)
+                    continue;
+
+                const score = await bingSearch(searchQuery, valueList, cache);
+                if (score < THRESHOLD)
+                    continue;
+
+                patterns.push({
+                    template: searchQuery.replace('${value}', '${p_' + lastProp + '}').replace(/ +/g, ' '),
+                    score
+                });
+            }
         }
-
-        return {
-            template: pattern.replace('${value}', '${p_' + propertyName + '}'),
-            score: await bingSearch(pattern, valueList, cache)
-        };
-    }))).filter((pattern) => pattern !== null && pattern.score >= THRESHOLD);
+    }));
 
     patterns.sort((p1, p2) => p2.score - p1.score);
 
     if (patterns.length === 0)
         return;
 
-    const dot = propertyName.lastIndexOf('.');
-    const lastProp = propertyName.substring(dot+1);
     dataset.examples.push(new Ast.Example(-1, 'query', { ['p_' + lastProp]: propertyType }, new Ast.Table.Filter(
         new Ast.Table.Invocation(new Ast.Invocation(new Ast.Selector.Device('org.schema', null, null), functionDef.name, [], functionDef), functionDef),
         new Ast.BooleanExpression.Atom(propertyName, operator === '==' && propertyType.isString ? '=~' : operator, Ast.Value.VarRef('p_' + lastProp)), null
@@ -263,40 +327,44 @@ async function main(args) {
             throw e;
     }
 
-    const data = JSON.parse(await util.promisify(fs.readFile)(args.data, { encoding: 'utf8' }));
-    const library = ThingTalk.Grammar.parse(await util.promisify(fs.readFile)(args.thingpedia, { encoding: 'utf8' }));
-    assert(library.isLibrary && library.classes.length === 1 && library.classes[0].kind === 'org.schema');
+    try {
+        const data = JSON.parse(await util.promisify(fs.readFile)(args.data, { encoding: 'utf8' }));
+        const library = ThingTalk.Grammar.parse(await util.promisify(fs.readFile)(args.thingpedia, { encoding: 'utf8' }));
+        assert(library.isLibrary && library.classes.length === 1 && library.classes[0].kind === 'org.schema');
 
-    const classDef = library.classes[0];
-    const queryDef = classDef.queries[table];
-    const rng = seedrandom.alea(args.random_seed);
-    const dataset = new Ast.Dataset('org.schema.' + table, 'en', [], {});
+        const classDef = library.classes[0];
+        const queryDef = classDef.queries[table];
+        const rng = seedrandom.alea(args.random_seed);
+        const dataset = new Ast.Dataset('org.schema.' + table, 'en', [], {});
+        const ppdb = args.ppdb ? await BinaryPPDB.mapFile(args.ppdb) : null;
 
-    const seen = new Set;
-    for (const argDef of queryDef.iterateArguments()) {
-        if (argDef.name === 'name')
-            continue;
-        if (argDef.type.isCompound)
-            continue;
-        if (seen.has(argDef.name))
-            continue;
-        seen.add(argDef.name);
-        const valueList = sample(table, data, argDef.name, argDef.type, rng);
-        if (valueList.length === 0)
-            continue;
+        const seen = new Set;
+        for (const argDef of queryDef.iterateArguments()) {
+            if (argDef.name === 'name')
+                continue;
+            if (argDef.type.isCompound)
+                continue;
+            if (seen.has(argDef.name))
+                continue;
+            seen.add(argDef.name);
+            const valueList = sample(table, data, argDef.name, argDef.type, rng);
+            if (valueList.length === 0)
+                continue;
 
-        await applyPatterns(queryDef, argDef, valueList, EQUAL_PATTERNS, '==', dataset, cache);
-        if (argDef.type.isNumeric()) {
-            await applyPatterns(queryDef, argDef, valueList, COMPARATIVE_MORE_PATTERNS, '>=', dataset, cache);
-            await applyPatterns(queryDef, argDef, valueList, COMPARATIVE_LESS_PATTERNS, '<=', dataset, cache);
+            await applyPatterns(ppdb, queryDef, argDef, valueList, EQUAL_PATTERNS, '==', dataset, cache);
+            if (argDef.type.isNumeric()) {
+                await applyPatterns(ppdb, queryDef, argDef, valueList, COMPARATIVE_MORE_PATTERNS, '>=', dataset, cache);
+                await applyPatterns(ppdb, queryDef, argDef, valueList, COMPARATIVE_LESS_PATTERNS, '<=', dataset, cache);
+            }
         }
+
+        args.output.end(dataset.prettyprint());
+        await StreamUtils.waitFinish(args.output);
+    } finally {
+        await util.promisify(fs.writeFile)(args.cache, JSON.stringify(cache, undefined, 2), { encoding: 'utf8' });
     }
-
-    await util.promisify(fs.writeFile)(args.cache, JSON.stringify(cache), { encoding: 'utf8' });
-
-    args.output.end(dataset.prettyprint());
-    await StreamUtils.waitFinish(args.output);
 }
+
 module.exports = {
     initArgparse(subparsers) {
         const parser = subparsers.addParser('webqa-auto-primitive', {
@@ -323,6 +391,10 @@ module.exports = {
             required: false,
             defaultValue: './auto-primitive-cache.json',
             help: 'Cache results of Bing search in this file.'
+        });
+        parser.addArgument('--ppdb', {
+            required: false,
+            help: 'Path to the compiled binary PPDB file',
         });
         parser.addArgument('--random-seed', {
             defaultValue: 'almond is awesome',
