d12ad8caac104dfb91c8175e3abfa085b0e5a512
Silei <silei@stanford.edu>
2020-05-24 23:10:01 -0700

    Bart annotation extraction

--- tool/lib/webqa-canonical-annotator.js
+++ tool/lib/webqa-canonical-annotator.js
@@ -14,11 +14,11 @@ const path = require('path');
 const child_process = require('child_process');
 const utils = require('../../lib/utils');
 
+const AnnotationExtractor = require('./webqa-template-extractor');
 const { makeLookupKeys } = require('../../lib/sample-utils');
 const { PROPERTY_CANONICAL_OVERRIDE } = require('./webqa-manual-annotations');
 
 const ANNOTATED_PROPERTIES = Object.keys(PROPERTY_CANONICAL_OVERRIDE);
-
 
 // extract entity type from type
 function typeToEntityType(type) {
@@ -166,8 +166,10 @@ class AutoCanonicalAnnotator {
 
             const {synonyms, adjectives} = JSON.parse(stdout);
             this._updateCanonicals(synonyms, adjectives);
-            if (this.gpt2_paraphraser)
-                await this._gpt2UpdateCanonicals(synonyms, queries);
+            if (this.gpt2_paraphraser) {
+                const extractor = new AnnotationExtractor(this.class, this.queries, this.gpt2_paraphraser_model, this.options);
+                await extractor.run(synonyms, queries);
+            }
         }
 
         return this.class;
@@ -230,122 +232,6 @@ class AutoCanonicalAnnotator {
                             canonicals[type].push(candidate);
                     }
                 }
-            }
-        }
-    }
-
-    async _paraphrase(input) {
-        const args = [
-            `run-paraphrase`,
-            `--model_type`, `gpt2`,
-            `--model_name_or_path`, this.gpt2_paraphraser_model,
-            `--length`, `15`,
-            `--temperature`, `0.0`,
-            `--repetition_penalty`, `1.0`,
-            `--num_samples`, `1`,
-            `--input_column`, `1`,
-            `--batch_size`, `8`,
-            `--stop_tokens`, `</paraphrase>`, `?`
-        ];
-        const child = child_process.spawn(`genienlp`, args, { stdio: ['pipe', 'pipe', 'inherit'] });
-
-        const output = util.promisify(fs.writeFile);
-        if (this.options.debug)
-            await output(`./gpt2-paraphraser-in.tsv`, input);
-
-        const stdout = await new Promise((resolve, reject) => {
-            child.stdin.write(input);
-            child.stdin.end();
-            child.on('error', reject);
-            child.stdout.on('error', reject);
-            child.stdout.setEncoding('utf8');
-            let buffer = '';
-            child.stdout.on('data', (data) => {
-                buffer += data;
-            });
-            child.stdout.on('end', () => resolve(buffer));
-        });
-
-        console.log(stdout);
-
-        if (this.options.debug)
-            await output(`./gpt2-paraphraser-out.json`, JSON.stringify(JSON.parse(stdout), null, 2));
-
-        return JSON.parse(stdout);
-    }
-
-    async _gpt2UpdateCanonicals(synonyms, queries) {
-        function generateGpt2Input(candidates) {
-            const input = [];
-            for (let category in candidates) {
-                for (let canonical in candidates[category]) {
-                    for (let sentence of candidates[category][canonical])
-                        input.push(`${input.length}\t${sentence}`);
-                }
-            }
-            return input;
-        }
-
-        function extractCanonical(origin, paraphrases, values, query_canonical) {
-            const canonical = {};
-            let value = values.find((v) => origin.includes(v));
-            if (!value) {
-                // base canonical
-                return canonical;
-            }
-            value = value.toLowerCase();
-
-            for (let paraphrase of paraphrases) {
-                if (!paraphrase.includes(value))
-                    continue;
-
-                let prefix;
-                if (origin.startsWith('who '))
-                    prefix = 'who ';
-                else
-                    prefix = origin.slice(0, origin.indexOf(query_canonical) + query_canonical.length + 1);
-
-                if (!paraphrase.startsWith(prefix))
-                    continue;
-
-                const clause = paraphrase.slice(prefix.length);
-
-                if (clause.startsWith('with ')) {
-                    canonical['property'] = canonical['property'] || [];
-                    canonical['property'].push(clause.slice('with '.length).replace(value, '#'));
-                } else if (clause.startsWith('that ')) {
-                    canonical['verb'] = canonical['verb'] || [];
-                    canonical['verb'].push(clause.slice('that '.length).replace(value, '#'));
-                }
-            }
-            return canonical;
-        }
-
-
-        for (let qname of this.queries) {
-            let query_canonical = queries[qname]['canonical'];
-            for (let arg in synonyms[qname]) {
-                let canonicals = this.class.queries[qname].getArgument(arg).metadata.canonical;
-                if (arg === 'id' || Object.keys(synonyms[qname][arg]).length === 0)
-                    continue;
-
-                let input = generateGpt2Input(synonyms[qname][arg]);
-                let output = await this._paraphrase(input.join('\n'));
-                let values = queries[qname]['args'][arg]['values'];
-                for (let i = 0; i < input.length; i++) {
-                    let newCanonicals = extractCanonical(input[i].split('\t')[1], output[i], values, query_canonical);
-                    for (let typeNewCanonical in newCanonicals) {
-                        for (let newCanonical of newCanonicals[typeNewCanonical]) {
-                            if (canonicals[typeNewCanonical].includes(newCanonical))
-                                continue;
-                            if (newCanonical.endsWith(' #') && canonicals[typeNewCanonical].includes(newCanonicals.slice(0, -2)))
-                                continue;
-                            canonicals[typeNewCanonical].push(newCanonical);
-                        }
-                    }
-                }
-
-
             }
         }
     }
