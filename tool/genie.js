b5f8263946b32bf1587593cb8311bf52b6e7c93a
Giovanni Campagna <gcampagn@cs.stanford.edu>
2020-05-31 16:30:00 -0700

    Add tool to extract the slots predicted by a dialogue model
    To compare with the existing test sets

--- tool/genie.js
+++ tool/genie.js
@@ -61,7 +61,8 @@ const subcommands = {
     'webqa-update-canonicals': require('./webqa-update-canonicals'),
 
     'auto-annotate-multiwoz': require('./auto-annotate-multiwoz'),
-    'convert-thingtalk-to-multidst': require('./convert-thingtalk-to-multidst')
+    'convert-thingtalk-to-multidst': require('./convert-thingtalk-to-multidst'),
+    'extract-predicted-slots': require('./extract-predicted-slots')
 };
 
 async function main() {
