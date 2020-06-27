0c3aa0f4df47a3b28901e6651dab5287ff8be334
Giovanni Campagna <gcampagn@cs.stanford.edu>
2020-06-27 11:36:38 -0700

    Add an "assistant" command
    For testing and demos, import almond-cmdline as "genie assistant"

---
+++ tool/assistant.js
@@ -0,0 +1,276 @@
+// -*- mode: js; indent-tabs-mode: nil; js-basic-offset: 4 -*-
+//
+// This file is part of Genie
+//
+// Copyright 2016-2020 The Board of Trustees of the Leland Stanford Junior University
+//
+// Author: Giovanni Campagna <gcampagn@cs.stanford.edu>
+//
+// See COPYING for details
+"use strict";
+
+const path = require('path');
+const readline = require('readline');
+
+const Engine = require('../lib/engine');
+const Platform = require('./lib/cmdline-platform');
+
+const THINGPEDIA_URL = 'https://thingpedia.stanford.edu/thingpedia';
+const NL_SERVER_URL = 'https://almond-nl.stanford.edu';
+
+class LocalUser {
+    constructor() {
+        this.id = process.getuid();
+        this.account = 'local:'+this.id;
+        this.name = 'Local User';
+    }
+}
+
+class CommandLineDelegate {
+    constructor(rl) {
+        this._rl = rl;
+    }
+
+    setHypothesis(hypothesis) {
+        // go to beginning of line
+        this._rl.write('', { ctrl: true, name: 'a' });
+        // erase line
+        this._rl.write('', { ctrl: true, name: 'k' });
+        // write the new hypothesis
+        this._rl.write(hypothesis);
+    }
+
+    addMessage(msg) {
+        switch (msg.type) {
+        case 'text':
+        case 'result':
+            console.log('>> ' + msg.text);
+            break;
+
+        case 'picture':
+            console.log('>> picture: ' + msg.url);
+            break;
+
+        case 'rdl':
+            console.log('>> rdl: ' + msg.rdl.displayTitle + ' ' + (msg.rdl.callback || msg.rdl.webCallback));
+            break;
+
+        case 'choice':
+            console.log('>> choice ' + msg.idx + ': ' + msg.title);
+            break;
+
+        case 'link':
+            console.log('>> link: ' + msg.title + ' ' + msg.url);
+            break;
+
+        case 'button':
+            console.log('>> button: ' + msg.title + ' ' + JSON.stringify(msg.json));
+            break;
+
+        case 'ask-special':
+            console.log('>> ask special ' + msg.askSpecialType);
+            break;
+        }
+    }
+}
+
+class CommandLineHandler {
+    constructor(engine, conversation, rl) {
+        this._engine = engine;
+        this._conversation = conversation;
+
+        this._rl = rl;
+        this._rl.on('line', this._onLine.bind(this));
+        this._rl.on('SIGINT', this._quit.bind(this));
+
+        this._oauthKind = null;
+        this._oauthSession = {};
+    }
+
+    _quit() {
+        console.log('Bye\n');
+        this._rl.close();
+        this._engine.stop();
+    }
+
+    _help() {
+        console.log('Available commands:');
+        console.log('\\q : quit');
+        console.log('\\r <json-or-nn-tt> : send parsed command to Genie');
+        console.log('\\y : answer yes');
+        console.log('\\n : answer no');
+        console.log('\\c <number> : make a choice');
+        console.log('\\t <code> : send ThingTalk to Genie');
+        console.log('\\a list : list apps');
+        console.log('\\a stop [<uuid> | all] : stop app');
+        console.log('\\d list : list devices');
+        console.log('\\d start-oauth <kind> : start oauth');
+        console.log('\\d complete-oauth <url> : finish oauth');
+        console.log('\\d update <kind> : update devices');
+        console.log('\\= <pref> : show a preference value');
+        console.log('\\= <pref> <value> : set a preference value');
+        console.log('\\? or \\h : show this help');
+        console.log('Any other command is interpreted as an English sentence and sent to Genie');
+    }
+
+    _runAppCommand(cmd, param) {
+        if (cmd === 'list') {
+            this._engine.apps.getAllApps().forEach((app) => {
+                console.log('- ' + app.uniqueId + ' ' + app.name + ': ' + app.description);
+            });
+        } else if (cmd === 'stop') {
+            if (param === 'all') {
+                for (let app of this._engine.apps.getAllApps())
+                    this._engine.apps.removeApp(app);
+            } else {
+                var app = this._engine.apps.getApp(param);
+                if (!app)
+                    console.log('No app with ID ' + param);
+                else
+                    this._engine.apps.removeApp(app);
+            }
+        }
+    }
+
+    _runPrefCommand(param, value) {
+        const prefs = this._engine.platform.getSharedPreferences();
+        if (value)
+            console.log(prefs.set(param, JSON.parse(value)) ? "ok" : "failed to set");
+        else
+            console.log(prefs.get(param));
+    }
+
+    _runDeviceCommand(cmd, param) {
+        if (cmd === 'list') {
+            this._engine.devices.getAllDevices().forEach((dev) => {
+                console.log('- ' + dev.uniqueId + ' (' + dev.kind +') ' + dev.name + ': ' + dev.description);
+            });
+        } else if (cmd === 'start-oauth' || cmd === 'start-oauth2') {
+            this._oauthKind = param;
+            return this._engine.devices.addFromOAuth(param).then(([redirect, session]) => {
+                this._oauthSession = session;
+                console.log(redirect);
+            });
+        } else if (cmd === 'complete-oauth' || cmd === 'complete-oauth2') {
+            return this._engine.devices.completeOAuth(this._oauthKind, param, this._oauthSession);
+        } else if (cmd === 'update' || cmd === 'upgrade') {
+            return this._engine.devices.updateDevicesOfKind(param);
+        }
+
+        return Promise.resolve();
+    }
+
+    _handleSlashR(line) {
+        line = line.trim();
+        if (line.startsWith('{'))
+            return this._conversation.handleParsedCommand(JSON.parse(line));
+        else
+            return this._conversation.handleParsedCommand({ code: line.split(' '), entities: {} });
+    }
+
+    _onLine(line) {
+        Promise.resolve().then(() => {
+            if (line[0] === '\\') {
+                if (line[1] === 'q')
+                    return this._quit();
+                else if (line[1] === '?' || line === 'h')
+                    return this._help();
+                else if (line[1] === 'r')
+                    return this._handleSlashR(line.substr(3));
+                else if (line[1] === 't')
+                    return this._conversation.handleThingTalk(line.substr(3));
+                else if (line[1] === 'c')
+                    return this._conversation.handleParsedCommand({ code: ['bookkeeping', 'choice', line.substr(3)], entities: {} });
+                else if (line[1] === 'y')
+                    return this._conversation.handleParsedCommand({ code: ['bookkeeping', 'special', 'special:yes'], entities: {} });
+                else if (line[1] === 'n')
+                    return this._conversation.handleParsedCommand({ code: ['bookkeeping', 'special', 'special:no'], entities: {} });
+                else if (line[1] === 'a')
+                    return this._runAppCommand(...line.substr(3).split(' '));
+                else if (line[1] === 'd')
+                    return this._runDeviceCommand(...line.substr(3).split(' '));
+                else if (line[1] === '=')
+                    return this._runPrefCommand(...line.substr(3).split(' '));
+                else
+                    console.log('Unknown command ' + line[1]);
+            } else if (line.trim()) {
+                return this._conversation.handleCommand(line);
+            }
+
+            // quiet warning
+            return Promise.resolve();
+        }).then(() => {
+            this._rl.prompt();
+        });
+    }
+}
+
+module.exports = {
+    initArgparse(subparsers) {
+        const parser = subparsers.addParser('assistant', {
+            addHelp: true,
+            description: "Test/demo the assistant interactively."
+        });
+        parser.addArgument('--workdir', {
+            required: false,
+            defaultValue: process.cwd(),
+            help: 'Directory where to store the assistant database and other files (defaults to the current working directory).'
+        });
+        parser.addArgument(['-l', '--locale'], {
+            required: false,
+            defaultValue: 'en-US',
+            help: `BGP 47 locale tag of the language to use for the assistant (defaults to 'en-US', English)`
+        });
+        parser.addArgument('--thingpedia-url', {
+            required: false,
+            defaultValue: THINGPEDIA_URL,
+            help: 'URL of Thingpedia to use.'
+        });
+        parser.addArgument('--thingpedia-dir', {
+            required: false,
+            help: 'Path to a directory containing Thingpedia device definitions (overrides --thingpedia-url).'
+        });
+        parser.addArgument('--nlu-server', {
+            required: false,
+            defaultValue: NL_SERVER_URL,
+            help: 'NLP server URL to use for NLU (can be a file:/// URL).'
+        });
+        parser.addArgument('--nlg-server', {
+            required: false,
+            help: 'NLP server URL to use for NLG; must be specified to use neural NLG.'
+        });
+        parser.addArgument('--wake-word-model', {
+            required: false,
+            help: 'Path to a wake-word model to use.'
+        });
+    },
+
+    async execute(args) {
+        const platform = new Platform(path.resolve(args.workdir), args.locale, args.thingpediaUrl, args.wake_word_model);
+        const prefs = platform.getSharedPreferences();
+        prefs.set('developer-dir', args.thingpedia_dir);
+        prefs.set('experimental-contextual-model', true);
+        prefs.set('experimental-use-neural-nlg', !!args.nlg_server);
+        const engine = new Engine(platform);
+
+        const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+        rl.setPrompt('$ ');
+
+        await engine.open();
+        const conversation = await engine.assistant.getOrOpenConversation('main', new LocalUser(), {
+            nluServerUrl: args.nlu_server,
+            nlgServerUrl: args.nlg_server,
+            debug: false,
+            showWelcome: true
+        });
+        await conversation.addOutput(new CommandLineDelegate(rl));
+
+        new CommandLineHandler(engine, conversation);
+        await conversation.start();
+        rl.prompt();
+
+        await engine.run();
+
+        await engine.close();
+    }
+};
---
+++ tool/lib/cmdline-platform.js
@@ -0,0 +1,299 @@
+// -*- mode: js; indent-tabs-mode: nil; js-basic-offset: 4 -*-
+//
+// This file is part of Genie
+//
+// Copyright 2015-2020 The Board of Trustees of the Leland Stanford Junior University
+//
+// Author: Giovanni Campagna <gcampagn@cs.stanford.edu>
+//
+// See COPYING for details
+"use strict";
+
+// cmdline platform
+
+const Tp = require('thingpedia');
+
+const fs = require('fs');
+const os = require('os');
+const path = require('path');
+const child_process = require('child_process');
+const stream = require('stream');
+const util = require('util');
+const Gettext = require('node-gettext');
+let PulseAudio;
+try {
+    PulseAudio = require('pulseaudio2');
+} catch(e) {
+    PulseAudio = null;
+}
+let snowboy;
+try {
+    snowboy = require('snowboy');
+} catch(e) {
+    snowboy = null;
+}
+
+const _unzipApi = {
+    unzip(zipPath, dir) {
+        var args = ['-uo', zipPath, '-d', dir];
+        return util.promisify(child_process.execFile)('/usr/bin/unzip', args, {
+            maxBuffer: 10 * 1024 * 1024 }).then(({ stdout, stderr }) => {
+            console.log('stdout', stdout);
+            console.log('stderr', stderr);
+        });
+    }
+};
+
+const _contentApi = {
+    getStream(url) {
+        return new Promise((resolve, reject) => {
+            if (url.startsWith('file:///')) {
+                const path = url.substring('file://'.length);
+                child_process.execFile('xdg-mime', ['query', 'filetype', path], (err, stdout, stderr) => {
+                    let stream = fs.createReadStream(path);
+                    if (err) {
+                        // ignore error if we failed to query the content type (e.g. if xdg-mime is not installed)
+                        stream.contentType = 'application/octet-stream';
+                    } else {
+                        stream.contentType = String(stdout).trim();
+                    }
+                    resolve(stream);
+                });
+            } else {
+                reject(new Error('Unsupported url ' + url));
+            }
+        });
+    }
+};
+
+function safeMkdirSync(dir) {
+    try {
+        fs.mkdirSync(dir);
+    } catch(e) {
+        if (e.code !== 'EEXIST')
+            throw e;
+    }
+}
+
+class SnowboyDetectorStream extends stream.Writable {
+    constructor(modelPath) {
+        super();
+
+        let models = new snowboy.Models();
+        const modelName = path.basename(modelPath);
+        models.add({
+             file: path.resolve(modelPath),
+             sensitivity: '0.6',
+
+             // remove '.pmdl' or '.umdl' extension
+             hotwords: modelName.substring(0, modelName.length-5)
+        });
+
+        this._detector = new snowboy.Detector({
+            resource: path.resolve(path.dirname(modelPath), 'common.res'),
+            models: models,
+            audioGain: 1.0,
+            applyFrontend: true,
+        });
+
+        this._detector.on('silence', () => {
+            this.emit('silence');
+        });
+        this._detector.on('sound', () => {
+            this.emit('sound');
+        });
+        this._detector.on('hotword', (index, hotword, buffer) => {
+            this.emit('wakeword', hotword);
+        });
+    }
+
+    _write(chunk, encoding, callback) {
+        this._detector.write(chunk, encoding, callback);
+    }
+}
+
+module.exports = class Platform extends Tp.BasePlatform {
+    // Initialize the platform code
+    // Will be called before instantiating the engine
+    constructor(homedir, locale, thingpediaUrl, snowboyPath) {
+        super();
+
+        this._locale = locale;
+        this._gettext = new Gettext();
+        this._gettext.setLocale(this._locale);
+
+
+        this._timezone = process.env.TZ;
+
+        this._filesDir = homedir;
+        this._cacheDir = path.resolve(homedir, 'cache');
+        safeMkdirSync(this._cacheDir);
+        this._prefs = new Tp.Helpers.FilePreferences(this._filesDir + '/prefs.db');
+
+        this._tpClient = new Tp.HttpClient(this, thingpediaUrl);
+
+        if (PulseAudio) {
+            this._pulse = new PulseAudio({
+                client: "genie-toolkit"
+            });
+        } else {
+            this._pulse = null;
+        }
+
+        if (snowboy && snowboyPath)
+            this._wakeWordDetector = new SnowboyDetectorStream(snowboyPath);
+        else
+            this._wakeWordDetector = null;
+    }
+
+    get type() {
+        return 'cmdline';
+    }
+
+    get locale() {
+        return this._locale;
+    }
+
+    get timezone() {
+        return this._timezone;
+    }
+
+    getPlatformDevice() {
+        return null;
+    }
+
+    // Check if we need to load and run the given thingengine-module on
+    // this platform
+    // (eg we don't need discovery on the cloud, and we don't need graphdb,
+    // messaging or the apps on the phone client)
+    hasFeature(feature) {
+        return true;
+    }
+
+    // Check if this platform has the required capability
+    // (eg. long running, big storage, reliable connectivity, server
+    // connectivity, stable IP, local device discovery, bluetooth, etc.)
+    //
+    // Which capabilities are available affects which apps are allowed to run
+    hasCapability(cap) {
+        switch(cap) {
+        case 'code-download':
+        case 'thingpedia-client':
+        case 'gettext':
+        case 'content-api':
+            return true;
+
+        case 'pulseaudio':
+        case 'sound':
+            return this._pulse !== null;
+
+        case 'wake-word-detector':
+            return this._wakeWordDetector !== null;
+
+        default:
+            return false;
+        }
+    }
+
+    // Retrieve an interface to an optional functionality provided by the
+    // platform
+    //
+    // This will return null if hasCapability(cap) is false
+    getCapability(cap) {
+        switch(cap) {
+        case 'code-download':
+            return _unzipApi;
+        case 'thingpedia-client':
+            return this._tpClient;
+        case 'gettext':
+            return this._gettext;
+        case 'content-api':
+            return _contentApi;
+        case 'sound':
+        case 'pulseaudio': // legacy name for "sound"
+            return this._pulse;
+        case 'wake-word-detector':
+            return this._wakeWordDetector;
+        default:
+            return null;
+        }
+    }
+
+    // Obtain a shared preference store
+    // Preferences are simple key/value store which is shared across all apps
+    // but private to this instance (tier) of the platform
+    // Preferences should be normally used only by the engine code, and a persistent
+    // shared store such as DataVault should be used by regular apps
+    getSharedPreferences() {
+        return this._prefs;
+    }
+
+    // Get a directory that is guaranteed to be writable
+    // (in the private data space for Android)
+    getWritableDir() {
+        return this._filesDir;
+    }
+
+    // Get a temporary directory
+    // Also guaranteed to be writable, but not guaranteed
+    // to persist across reboots or for long times
+    // (ie, it could be periodically cleaned by the system)
+    getTmpDir() {
+        return os.tmpdir();
+    }
+
+    // Get a directory good for long term caching of code
+    // and metadata
+    getCacheDir() {
+        return this._cacheDir;
+    }
+
+    // Get the filename of the sqlite database
+    getSqliteDB() {
+        return this._filesDir + '/sqlite.db';
+    }
+
+    getSqliteKey() {
+        return null;
+    }
+
+    getGraphDB() {
+        return this._filesDir + '/rdf.db';
+    }
+
+    // Get the Thingpedia developer key, if one is configured
+    getDeveloperKey() {
+        return this._prefs.get('developer-key');
+    }
+
+    // Change the Thingpedia developer key, if possible
+    // Returns true if the change actually happened
+    setDeveloperKey(key) {
+        return this._prefs.set('developer-key', key);
+    }
+
+    getOrigin() {
+        // pretend to be a local thingpedia server
+        // the user is expected to copy-paste oauth urls manually
+        return 'http://127.0.0.1:8080';
+    }
+
+    getCloudId() {
+        return this._prefs.get('cloud-id');
+    }
+
+    getAuthToken() {
+        return this._prefs.get('auth-token');
+    }
+
+    // Change the auth token
+    // Returns true if a change actually occurred, false if the change
+    // was rejected
+    setAuthToken(authToken) {
+        var oldAuthToken = this._prefs.get('auth-token');
+        if (oldAuthToken !== undefined && authToken !== oldAuthToken)
+            return false;
+        this._prefs.set('auth-token', authToken);
+        return true;
+    }
+};
