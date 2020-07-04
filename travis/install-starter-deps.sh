411b4285eda49383c713eaef5fb2a55288e07cb9
Giovanni Campagna <gcampagn@cs.stanford.edu>
2020-07-04 10:07:18 -0700

    travis: install the master version of genienlp
    Temporarily, until I test this and make a release.

--- travis/install-starter-deps.sh
+++ travis/install-starter-deps.sh
@@ -7,7 +7,7 @@ set -o pipefail
 srcdir=`dirname $0`/..
 srcdir=`realpath $srcdir`
 
-which genienlp >/dev/null 2>&1 || pip3 install --user 'genienlp==0.3.0'
+which genienlp >/dev/null 2>&1 || pip3 install --user 'git+https://github.com/stanford-oval/genienlp@845e4a4b03e3902babc71b3edbc61bf99a5013ac#egg=genienlp'
 which genienlp
 
 yarn global add thingpedia-cli
