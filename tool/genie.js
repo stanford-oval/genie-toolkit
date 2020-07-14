5d01e3531146ab5792fe47c4da6f75f4f5e3d3f2
Lucas Sato <lucasjks@gmail.com>
2020-07-13 21:32:34 -0300

    Get the bare minimum version working
    The annotator now correctly generates a .txt with the utterances (but
    not the annotations) in the correct format. This was mostly an exercise
    to find my way around the code, adapted from Giovanni's
    auto-annotate-multiwoz.js

--- tool/genie.js
+++ tool/genie.js
@@ -63,6 +63,7 @@ const subcommands = {
 
     'sgd-process-schema': require('./autoqa/sgd/process-schema'),
     'sgd-normalize-data': require('./autoqa/sgd/normalize-data'),
+    'sgd-auto-annotate': require('./autoqa/sgd/auto-annotate'),
 
     'wikidata-process-schema': require('./autoqa/wikidata/process-schema'),
 
