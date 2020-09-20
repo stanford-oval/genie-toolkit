b54380139f85a4e032721805d75940f6626d5c87
mehrad <mehrad@stanford.edu>
2020-09-19 22:20:30 -0700

    Do not override restaurant/ hotel names with openstreetmap
    You will lose localization otherwise

--- tool/autoqa/schemaorg/manual-annotations.js
+++ tool/autoqa/schemaorg/manual-annotations.js
@@ -563,12 +563,12 @@ const STRUCT_INCLUDE_THING_PROPERTIES = new Set([
 
 
 const STRING_FILE_OVERRIDES = {
-    'org.schema.Restaurant:Restaurant_name': 'org.openstreetmap:restaurant',
+    // 'org.schema.Restaurant:Restaurant_name': 'org.openstreetmap:restaurant',
     'org.schema.Person:Person_name': 'tt:person_full_name',
     'org.schema.Person:Person_alumniOf': 'tt:university_names',
     'org.schema.Person:Person_worksFor': 'tt:company_name',
     'org.schema.Person:Person_jobTitle': 'tt:job_title',
-    'org.schema.Hotel:Hotel_name': 'org.openstreetmap:hotel',
+    // 'org.schema.Hotel:Hotel_name': 'org.openstreetmap:hotel',
     'org.schema.Music:MusicRecording_byArtist': 'tt:song_artist',
     'org.schema.Music:MusicAlbum_byArtist': 'tt:song_artist',
     'org.schema.Music:MusicRecording_inAlbum': 'tt:song_album',
