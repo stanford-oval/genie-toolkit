b90e95076190d71859fe14aa9ee82001d92f842d
Giovanni Campagna <gcampagn@cs.stanford.edu>
2020-11-03 22:55:34 -0800

    Reduce logging spew

--- lib/sentence-generator/generator.ts
+++ lib/sentence-generator/generator.ts
@@ -1689,7 +1689,7 @@ function expandRule(charts : Charts,
     if (actualGenSize + prunedGenSize === 0)
         return;
     const newEstimatedPruneFactor = actualGenSize / (actualGenSize + prunedGenSize);
-    if (options.debug >= LogLevel.INFO && newEstimatedPruneFactor < 0.2)
+    if (options.debug >= LogLevel.GENERATION && newEstimatedPruneFactor < 0.2)
         console.log(`expand NT[${nonTermList[nonTermIndex]}] -> ${expansion.join(' ')} : semantic function only accepted ${(newEstimatedPruneFactor*100).toFixed(1)}% of derivations`);
 
     const elapsed = Date.now() - now;
