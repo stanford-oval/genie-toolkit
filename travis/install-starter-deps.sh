ddd94c3082ee0407f576ff640f408fc6084f5aba
Giovanni Campagna <gcampagn@cs.stanford.edu>
2020-12-19 23:22:36 -0800

    Switch to genienlp 0.6.0
    And reduce the number of training iterations in the tests, to make
    them run faster.

--- travis/install-starter-deps.sh
+++ travis/install-starter-deps.sh
@@ -7,7 +7,7 @@ set -o pipefail
 srcdir=`dirname $0`/..
 srcdir=`realpath $srcdir`
 
-which genienlp >/dev/null 2>&1 || pip3 install --user 'genienlp==0.5.0'
+which genienlp >/dev/null 2>&1 || pip3 install --user 'genienlp>=0.6.0a1'
 which genienlp
 
 npm install -g thingpedia-cli
