acb11734ade32ad11efe2512f2700b8c649101ce
Giovanni Campagna <gcampagn@cs.stanford.edu>
2021-01-02 15:55:38 -0800

    Import gettext translations from ThingTalk
    Going away from ThingTalk

--- po/update-pot.sh
+++ po/update-pot.sh
@@ -6,5 +6,5 @@ for f in $podir/../data/builtins/*/manifest.tt ; do
 	kind=$(basename $(dirname $f))
 	ts-node $podir/extract-translatable-annotations $f > $podir/tmp/$kind.js
 done
-find lib -name \*.js > po/POTFILES
-xgettext -kN_ -c -f po/POTFILES -x po/POTFILES.skip -o po/${npm_package_name}.pot --from-code UTF-8 --package-name ${npm_package_name} --package-version ${npm_package_version}
+find lib -name \*.js -or -name \*.ts > po/POTFILES
+xgettext -kN_ -c -f po/POTFILES -x po/POTFILES.skip -LJavaScript -o po/${npm_package_name}.pot --from-code UTF-8 --package-name ${npm_package_name} --package-version ${npm_package_version}
