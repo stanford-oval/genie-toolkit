c59b1fb023bac56269ced3d9f7cf62b4af85d562
Silei <silei@stanford.edu>
2021-03-18 20:30:06 -0700

    Remove duplicated unit tests

--- test/unit/index.js
+++ test/unit/index.js
@@ -40,7 +40,6 @@ async function do_test(array) {
 
 // test lib scripts
 do_test([
-    ('./test_bart_canonical_extractor'),
     ('./test_array_set'),
     ('./test_augment'),
     ('./test_bart_canonical_extractor'),
