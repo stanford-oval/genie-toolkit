a315e7d782376206987369175749fcb8537a9696
Giovanni Campagna <gcampagn@cs.stanford.edu>
2021-04-27 18:58:44 -0700

    Update genienlp used in tests and documentation

--- travis/install-starter-deps.sh
+++ travis/install-starter-deps.sh
@@ -8,7 +8,7 @@ srcdir=`dirname $0`/..
 srcdir=`realpath $srcdir`
 
 pip3 install --upgrade pip
-which genienlp >/dev/null 2>&1 || pip3 install --user 'git+https://github.com/stanford-oval/genienlp@8f48731ed1e907f2c2a469922d9b86eb8baa7631#egg=genienlp'
+which genienlp >/dev/null 2>&1 || pip3 install --user 'genienlp==v0.6.0a4'
 which genienlp
 
 mkdir -p $srcdir/test/embeddings
