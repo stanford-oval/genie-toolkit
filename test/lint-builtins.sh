9fd436927fd4f03091ff8dc1d2b492cd41174670
Giovanni Campagna <gcampagn@cs.stanford.edu>
2021-06-28 17:39:33 -0700

    Add test script to automatically lint builtin devices
    Let's catch errors before I update dependencies in thingpedia-common-devices,
    without waiting for long travis cycles...

---
+++ test/lint-builtins.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -e
+set -x
+
+for folder in ./data/builtins/* ; do
+    node ./dist/tool/genie.js lint-device \
+        --manifest $folder/manifest.tt --dataset $folder/dataset.tt
+done
