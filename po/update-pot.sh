6ab9e5133baa6a6860706598fd5c86e78a9b6a79
mehrad <mehrad@stanford.edu>
2021-10-13 16:17:47 -0700

    Add scripts for machine-translating po files

--- po/update-pot.sh
+++ po/update-pot.sh
@@ -5,11 +5,15 @@ set -x
 
 make
 podir=`dirname $0`
-find lib/ tool/ -name \*.js -or -name \*.ts > po/POTFILES
+
+# remove double-slashes (caused by old BSD find) and sort lines
+find lib/ tool/ -name \*.js -or -name \*.ts | sed 's@//@/@' | sort > po/POTFILES
+
 mkdir -p $podir/tmp
 for f in $podir/../data/builtins/*/manifest.tt ; do
-	kind=$(basename $(dirname $f))
+	kind=$(basename "$(dirname $f))"
 	node --experimental_worker --max_old_space_size=8500 `dirname "$0"`/../dist/tool/genie.js extract-translatable-annotations $f > $podir/tmp/$kind.js
 	echo $podir/tmp/$kind.js >> po/POTFILES
 done
+
 xgettext -kN_ -c -f po/POTFILES -x po/POTFILES.skip -LJavaScript -o po/${npm_package_name}.pot --from-code UTF-8 --package-name ${npm_package_name} --package-version ${npm_package_version}
