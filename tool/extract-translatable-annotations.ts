ac8e004aa90b6993d9c7a440eae3283bf643dd83
Giovanni Campagna <gcampagn@cs.stanford.edu>
2021-10-24 20:11:49 -0700

    Update tool to merge annotations
    Factor out the code to walk annotations into a common module,
    and use (p)gettext consistently to merge the annotations. This
    ensures that the translations are actually compatible with the
    tools that process PO files.

--- tool/extract-translatable-annotations.ts
+++ tool/extract-translatable-annotations.ts
@@ -25,9 +25,9 @@ import * as fs from 'fs';
 import { promises as pfs } from 'fs';
 import * as ThingTalk from 'thingtalk';
 
-import * as I18n from '../lib/i18n';
 import { stringEscape } from '../lib/utils/escaping';
-import { Choice, Replaceable } from '../lib/utils/template-string';
+
+import { processLibrary } from './lib/extract-translatable';
 
 export function initArgparse(subparsers : argparse.SubParser) {
     const parser = subparsers.add_parser('extract-translatable-annotations', {
@@ -43,71 +43,7 @@ export function initArgparse(subparsers : argparse.SubParser) {
     });
 }
 
-let output : fs.WriteStream;
-
-function extract(key : string, str : unknown) {
-    if (typeof str === 'boolean' || typeof str === 'number')
-        return;
-    if (typeof str === 'string') {
-        output.write(`/* ${key} */\n`);
-        output.write(`let x = _(${stringEscape(str)});\n`);
-    } else if (Array.isArray(str)) {
-        for (let i = 0; i < str.length; i++)
-            extract(`${key}[${i}]`, str[i]);
-    } else if (typeof str === 'object' && str !== null) {
-        for (const subkey in str) {
-            if (subkey === 'type' || subkey === 'default')
-                continue;
-            extract(`${key}.${subkey}`, str[subkey as keyof typeof str]);
-        }
-    } else {
-        throw new TypeError(`Invalid translatable entry #_[${key}=${str}]`);
-    }
-}
-
-function makeChoice(choices : Replaceable[]) {
-    assert(choices.length > 0);
-    if (choices.length === 1)
-        return choices[0].toString();
-    return new Choice(choices).toString();
-}
-
-function extractFunctionCanonical(langPack : I18n.LanguagePack, key : string, fndef : ThingTalk.Ast.FunctionDef, str : unknown) {
-    const normalized = langPack.preprocessFunctionCanonical(str, fndef.functionType, 'user', fndef.is_list);
-
-    output.write(`/* ${key} */\n`);
-    output.write(`let x = pgettext(${stringEscape(key)}, ${stringEscape(makeChoice(normalized))});\n`);
-}
-
-function extractParameterCanonical(langPack : I18n.LanguagePack, key : string, str : unknown) {
-    const normalized = langPack.preprocessParameterCanonical(str, 'user');
-
-    output.write(`/* ${key}.default */\n`);
-    output.write(`/* Translators: this is the POS to use as default for agent replies, it is a POS tag, it should not be translated */\n`);
-    output.write(`let x = pgettext(${stringEscape(key + '.default')}, ${stringEscape(normalized.default)});\n`);
-
-    for (const subkey of ['base', 'base_projection', 'argmin', 'argmax', 'projection', 'filter'] as const) {
-        if (normalized[subkey].length === 0)
-            continue;
-        const fullkey = `${key}.${subkey}`;
-        output.write(`/* ${fullkey} */\n`);
-        output.write(`let x = pgettext(${stringEscape(fullkey)}, ${stringEscape(makeChoice(normalized[subkey]))});\n`);
-    }
-
-    for (const enum_ in normalized.enum_filter) {
-        const enum_options = normalized.enum_filter[enum_];
-        if (enum_options.length === 0)
-            continue;
-
-        const fullkey = `${key}.enum.${enum_}`;
-        output.write(`/* ${fullkey} */\n`);
-        output.write(`let x = pgettext(${stringEscape(fullkey)}, ${stringEscape(makeChoice(enum_options))});\n`);
-    }
-}
-
 export async function execute(args : any) {
-    const langPack = I18n.get('en-US');
-
     const code = (await pfs.readFile(args.input_file)).toString();
     const parsed = ThingTalk.Syntax.parse(code, ThingTalk.Syntax.SyntaxType.Normal, {
         locale: 'en-US',
@@ -115,50 +51,17 @@ export async function execute(args : any) {
     });
     assert(parsed instanceof ThingTalk.Ast.Library);
 
-    output = fs.createWriteStream(args.output);
+    const output = fs.createWriteStream(args.output);
 
-    // parse manifest
-    for (const _class of parsed.classes) {
-        for (const key in _class.nl_annotations)
-            extract(`${key}`, _class.nl_annotations[key]);
-        for (const what of ['queries', 'actions'] as const) {
-            for (const name in _class[what]) {
-                const fndef = _class[what][name];
-                for (const key in fndef.nl_annotations) {
-                    if (key === 'canonical')
-                        extractFunctionCanonical(langPack, `${what}.${name}.${key}`, fndef, fndef.nl_annotations[key]);
-                    else
-                        extract(`${what}.${name}.${key}`, fndef.nl_annotations[key]);
-                }
-
-                for (const argname of _class[what][name].args) {
-                    const arg = _class[what][name].getArgument(argname)!;
-
-                    for (const key in arg.nl_annotations) {
-                        if (key === 'canonical')
-                            extractParameterCanonical(langPack, `${what}.${name}.args.${argname}.${key}`, arg.nl_annotations[key]);
-                        else
-                            extract(`${what}.${name}.args.${argname}.${key}`, arg.nl_annotations[key]);
-                    }
-
-                    // handle enums
-                    if (arg.type.isEnum)
-                        extract(`${what}.${name}.args.${argname}.enum`, arg.type);
-                }
-            }
-        }
+    for (const string of processLibrary(parsed)) {
+        output.write(`/* ${string.key} */\n`);
+        if (string.comment)
+            output.write(`/* ${string.comment} */\n`);
+        if (string.context)
+            output.write(`let x = pgettext(${stringEscape(string.context)}, ${stringEscape(string.object[string.field])});\n`);
+        else
+            output.write(`let x = _(${stringEscape(string.object[string.field])});\n`);
     }
 
-    // parse dataset
-    for (const _class of parsed.datasets) {
-        for (const [ex_id, ex] of _class.examples.entries()) {
-            for (const [uttr_id, uttr] of ex.utterances.entries()) {
-                const key = `${_class.name}.${ex_id}.${uttr_id}`;
-                output.write(`/* ${key} */\n`);
-                output.write(`let x = _(${stringEscape(uttr)});\n`);
-            }
-        }
-    }
-
-
+    output.end();
 }
--- tool/translate-schema-annotations.ts
+++ tool/translate-schema-annotations.ts
@@ -19,12 +19,15 @@
 // Author: Mehrad Moradshahi <mehrad@cs.stanford.edu>
 
 import * as argparse from 'argparse';
+import Gettext from 'node-gettext';
 import * as gettextParser from 'gettext-parser';
 import assert from 'assert';
 import * as fs from 'fs';
 import { promises as pfs } from 'fs';
 import * as ThingTalk from 'thingtalk';
+
 import * as StreamUtils from "../lib/utils/stream-utils";
+import { processLibrary } from './lib/extract-translatable';
 
 export function initArgparse(subparsers : argparse.SubParser) {
     const parser = subparsers.add_parser('translate-schema-annotations', {
@@ -44,79 +47,6 @@ export function initArgparse(subparsers : argparse.SubParser) {
     });
 }
 
-const translationDict = new Map<string, string>();
-
-function* returnAllKeys(key : string, str : unknown) : any {
-    if (typeof str === 'string') {
-        yield key;
-    } else if (Array.isArray(str)) {
-        for (let i = 0; i < str.length; i++)
-            yield* returnAllKeys(`${key}[${i}]`, str[i]);
-    } else if (typeof str === 'object' && str !== null) {
-        for (const subkey in str) {
-            if (subkey === 'type' || subkey === 'default')
-                continue;
-            yield* returnAllKeys(`${key};${subkey}`, str[subkey as keyof typeof str]);
-        }
-    } else {
-        yield null;
-    }
-}
-
-function processEnum(arg : any, iterKey : string,) {
-    const valList : Array<string | undefined> = [];
-    assert(typeof arg.type === 'object');
-    for (const k of returnAllKeys(iterKey, arg.type)) {
-        if (k === null)
-            continue;
-        valList.push(translationDict.get(k));
-    }
-
-    const keys : string[] = arg.type.entries;
-    assert(keys.length === valList.length);
-
-    const valueEnum = Object.fromEntries(keys.map((_, i) => [keys[i], [valList[i]]]));
-
-    if (!arg.metadata['canonical'])
-        arg.metadata['canonical'] = { 'value_enum': valueEnum } ;
-    else if (typeof arg.metadata['canonical'] === 'string' || Array.isArray(arg.metadata['canonical']))
-        arg.metadata['canonical'] = { 'base': arg.metadata['canonical'], 'value_enum': valueEnum };
-    else
-        arg.metadata['canonical']['value_enum'] = valueEnum;
-
-}
-
-function process(obj : any, iterKey : string) {
-    for (const key in obj) {
-        for (const k of returnAllKeys(iterKey + `.${key}`, obj[key])) {
-            if (k === null)
-                continue;
-            if (Array.isArray(obj[key])) {
-                let subKey = k.split('.').slice(-1);
-                // TODO fix
-                if (subKey.length !== 1) {
-                    console.log(subKey);
-                    continue;
-                }
-                subKey = subKey[0];
-                const [, , id_] = /(.+?)\[(\d+)\]/.exec(subKey)!;
-                obj[key][id_] = translationDict.get(k);
-            } else if (typeof obj[key] === 'object') {
-                let subKey = k.split(';').slice(1);
-                // TODO fix
-                if (subKey.length !== 1)
-                    console.log(subKey);
-                subKey = subKey[0];
-                const [, substr, id_] = /(.+?)\[(\d+)\]/.exec(subKey)!;
-                obj[key][substr][id_] = translationDict.get(k);
-            } else {
-                obj[key] = translationDict.get(k);
-            }
-        }
-    }
-
-}
-
 export async function execute(args : any) {
     const code = (await pfs.readFile(args.input_file)).toString();
     const parsed = ThingTalk.Syntax.parse(code, ThingTalk.Syntax.SyntaxType.Normal, {
@@ -125,50 +55,27 @@ export async function execute(args : any) {
     });
     assert(parsed instanceof ThingTalk.Ast.Library);
 
-    const pofile = gettextParser.po.parse(await pfs.readFile(args.po_file, { encoding: 'utf8' }));
+    const gt = new Gettext();
+    gt.textdomain('schema');
 
-    // get translations for the default context
-    const poObjects = pofile.translations[''];
-    for (const msgid in poObjects) {
-        const entry : gettextParser.GetTextTranslation = poObjects[msgid];
-        const refKeys : string[]|undefined = entry.comments?.reference?.split('\n');
-        const extKeys : string[]|undefined = entry.comments?.extracted?.split('\n');
-        for (const keys of [refKeys, extKeys]) {
-            if (!keys)
-                continue;
-            for (const k of keys) {
-                assert(entry.msgstr.length === 1);
-                translationDict.set(k, entry.msgstr[0]);
-            }
-        }
+    try {
+        const pofile = gettextParser.po.parse(await pfs.readFile(args.po_file, { encoding: 'utf8' }));
+        gt.setLocale(pofile.headers.Language);
+        gt.addTranslations(pofile.headers.Language, 'schema', pofile);
+    } catch(e : any) {
+        if (e.code !== 'ENOENT')
+            throw e;
+        // ignore if the file does not exist
+        // this allows using this script for languages that are not supported yet
+        // e.g. in a repository where some skills are translated and some are not
+        console.log(`WARNING: ${args.po_file} does not exist, no translation will be applied`);
     }
 
-    // handle manifest
-    for (const _class of parsed.classes) {
-        for (const key in _class.metadata)
-            _class.metadata[key] = translationDict.get(key);
-        for (const what of ['queries', 'actions'] as Array<'queries' | 'actions'>) {
-            for (const name in _class[what]) {
-                process(_class[what][name].metadata, `${what}.${name}`);
-                for (const argname of _class[what][name].args) {
-                    const arg = _class[what][name].getArgument(argname)!;
-                    process(arg.metadata, `${what}.${name}.args.${argname}`);
-
-                    if (arg.type.isEnum)
-                        processEnum(arg, `${what}.${name}.args.${argname}.enum`);
-                }
-            }
-        }
-    }
-
-    // handle dataset
-    for (const _class of parsed.datasets) {
-        for (const [ex_id, ex] of _class.examples.entries()) {
-            for (const [uttr_id, _] of ex.utterances.entries()) {
-                const key = `${_class.name}.${ex_id}.${uttr_id}`;
-                ex.utterances[uttr_id] = translationDict.get(key)!;
-            }
-        }
+    for (const entry of processLibrary(parsed)) {
+        if (entry.context)
+            entry.object[entry.field] = gt.pgettext(entry.context, entry.object[entry.field]);
+        else
+            entry.object[entry.field] = gt.gettext(entry.object[entry.field]);
     }
 
     if (args.output) {
