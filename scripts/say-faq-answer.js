8fceb287b4e04597b7b16e1afc65498a2bfbd106
nrser <neil@neilsouza.com>
2021-12-11 10:05:39 -0800

    Tweak faq text

--- scripts/say-faq-answer.js
+++ scripts/say-faq-answer.js
@@ -9,7 +9,8 @@ const BASE_URL = "https://nlp-staging.almond.stanford.edu/en-US/voice/tts?text="
 const name = process.argv[2];
 const filename = "data/builtins/org.thingpedia.builtin.thingengine.builtin/faq.yaml";
 const data = yaml.load(fs.readFileSync(filename, { encoding: "utf-8" }));
-const key = `about_almond_${name}`;
+// const key = `about_almond_${name}`;
+const key = name;
 const answer = data[key]["a"][0];
 
 const url = BASE_URL + encodeURIComponent(answer);
